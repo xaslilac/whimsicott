433302cb8b26282c86fd423d8737e26997a809d3
McKayla Washburn <mckayla@hey.com>
2022-05-10 21:06:54 -0600

    :)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { serve } from "std/http";
+import { $interface, $number, guard } from "succulent";
 
-function handler(req: Request): Response {
+const puppies = [
+	"August",
+	"Dot",
+	"Mady",
+	"Toby",
+];
+
+const $PuppiesRequestBody = $interface({
+	count: $number.that((n) => n < puppies.length),
+});
+
+async function handler(req: Request): Promise<Response> {
 	const url = new URL(req.url);
+
 	console.log(req.url);
+
+	if (url.pathname === "/api/puppies") {
+		try {
+			const body = await req.json();
+			guard(body, $PuppiesRequestBody);
+			return new Response(JSON.stringify(puppies.slice(0, body.count)));
+		} catch {
+			return new Response("Error", { status: 500 });
+		}
+	}
+
+	if (url.pathname === "/favicon.ico") {
+		return new Response("404 Not Found", { status: 404 });
+	}
+
 	if (url.pathname !== "/hi") {
 		return new Response("302 Redirect", {
 			status: 302,
