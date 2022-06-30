b8ebf25b8801aa962bf44fd34ea4da571531afba
McKayla Washburn <mckayla@hey.com>
2022-06-29 18:52:43 -0600

    :''')

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ async function handler(req: Request): Promise<Response> {
 		});
 	}
 
-	return new Response("Hi friend!", {
+	return new Response("Hi friend! Beep boop :)", {
 		headers: { "content-type": "text/html" },
 	});
 }
