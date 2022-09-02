c44eae78a9a300d469c9e134ff2768497bb699db
McKayla Washburn <mckayla@hey.com>
2022-09-02 15:34:39 -0600

    im bad

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ function doc(body: string) {
 		<!doctype html>
 		<html>
 		<head>
-		<title>Whimsicott<title>
+		<title>Whimsicott</title>
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet">
